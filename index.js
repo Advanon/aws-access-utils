c4b5bbc8b9f93339d471e954573f0cf60d164568
Ivan Martos <ivan.martos@advanon.com>
2019-04-03 15:14:55 +0200

    add index.js for convenience

---
+++ index.js
@@ -0,0 +1,10 @@
+exports.logger = require('./src/logger');
+exports.httpUtils = require('./src/httpUtils');
+exports.config = require('./src/config');
+exports.aws = require('./src/aws/aws');
+exports.arn = require('./src/aws/arn');
+exports.dynamoDb = require('./src/aws/dynamodb');
+exports.lambda = require('./src/aws/lambda');
+exports.ses = require('./src/aws/ses');
+exports.sns = require('./src/aws/sns');
+exports.stepFunctions = require('./src/aws/stepFunctions');
