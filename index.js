14ab90aa2b3d7db3816e4f0a1fff0b95b200fa87
Ivan Martos <ivan.martos@advanon.com>
2019-06-21 11:25:03 +0200

    exporting ssm

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
     config: require('./src/config'),
     aws: require('./src/aws/aws'),
     arn: require('./src/aws/arn'),
+    ssm: require('./src/aws/ssm'),
     dynamoDb: require('./src/aws/dynamodb'),
     ses: require('./src/aws/ses'),
     sns: require('./src/aws/sns'),
