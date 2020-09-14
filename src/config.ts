d61fc49da0fb2e3f31814a6c0b06701131793c39
Vlad Gramuzov <vlad.gramuzov@gmail.com>
2020-09-14 15:29:23 +0300

    chore(config): Use inexplicit return

--- src/config.ts
+++ src/config.ts
@@ -14,22 +14,14 @@ export const getAccountNumber = (): string => process.env.AWS_ACCOUNT_NUMBER!;
 
 export const getLoggerName = (): string | undefined => process.env.LOGGER_NAME;
 
-export const getLocalRegion = (): string => {
-  return process.env.LOCAL_REGION || DEFAULT_LOCAL_REGION;
-};
+export const getLocalRegion = (): string => process.env.LOCAL_REGION || DEFAULT_LOCAL_REGION;
 
-export const getLocalEndpointDynamoDB = (): string => {
-  return process.env.LOCAL_DYNAMODB_ENDPOINT || DEFAULT_LOCAL_URL_DYNAMODB;
-};
+export const getLocalEndpointDynamoDB = (): string =>
+  process.env.LOCAL_DYNAMODB_ENDPOINT || DEFAULT_LOCAL_URL_DYNAMODB;
 
-export const getLocalEndpointStepFunctions = (): string => {
-  return process.env.LOCAL_STEPFUNCTIONS_ENDPOINT || DEFAULT_LOCAL_URL_STEPFUNCTIONS;
-};
+export const getLocalEndpointStepFunctions = (): string =>
+  process.env.LOCAL_STEPFUNCTIONS_ENDPOINT || DEFAULT_LOCAL_URL_STEPFUNCTIONS;
 
-export const getLocalEndpointSns = (): string => {
-  return process.env.LOCAL_SNS_ENDPOINT || DEFAULT_LOCAL_URL_SNS;
-};
+export const getLocalEndpointSns = (): string => process.env.LOCAL_SNS_ENDPOINT || DEFAULT_LOCAL_URL_SNS;
 
-export const getLocalEndpointSes = (): string => {
-  return process.env.LOCAL_SES_ENDPOINT || DEFAULT_LOCAL_URL_SES;
-};
+export const getLocalEndpointSes = (): string => process.env.LOCAL_SES_ENDPOINT || DEFAULT_LOCAL_URL_SES;
