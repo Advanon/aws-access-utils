4836a1c53d0b6c5cf3d030ca7d7e8e8eeb1777c7
Vlad Gramuzov <vlad.gramuzov@gmail.com>
2020-09-15 16:48:13 +0300

    chore(config): Update local endpoint

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,6 @@
 const DEFAULT_LOCAL_REGION = 'us-east-1';
-const DEFAULT_LOCAL_URL_DYNAMODB = 'http://localhost:4566';
-const DEFAULT_LOCAL_URL_SNS = 'http://localhost:4566';
-const DEFAULT_LOCAL_URL_STEPFUNCTIONS = 'http://localhost:4566';
-const DEFAULT_LOCAL_URL_SES = 'http://localhost:4566';
+
+const DEFAULT_LOCAL_ENDPOINT = process.env.LOCALSTACK_HOSTNAME || 'http://localhost:4566';
 
 export const isOffline = (): boolean => !!process.env.IS_OFFLINE;
 
@@ -17,11 +15,11 @@ export const getLoggerName = (): string | undefined => process.env.LOGGER_NAME;
 export const getLocalRegion = (): string => process.env.LOCAL_REGION || DEFAULT_LOCAL_REGION;
 
 export const getLocalEndpointDynamoDB = (): string =>
-  process.env.LOCAL_DYNAMODB_ENDPOINT || DEFAULT_LOCAL_URL_DYNAMODB;
+  process.env.LOCAL_DYNAMODB_ENDPOINT || DEFAULT_LOCAL_ENDPOINT;
 
 export const getLocalEndpointStepFunctions = (): string =>
-  process.env.LOCAL_STEPFUNCTIONS_ENDPOINT || DEFAULT_LOCAL_URL_STEPFUNCTIONS;
+  process.env.LOCAL_STEPFUNCTIONS_ENDPOINT || DEFAULT_LOCAL_ENDPOINT;
 
-export const getLocalEndpointSns = (): string => process.env.LOCAL_SNS_ENDPOINT || DEFAULT_LOCAL_URL_SNS;
+export const getLocalEndpointSns = (): string => process.env.LOCAL_SNS_ENDPOINT || DEFAULT_LOCAL_ENDPOINT;
 
-export const getLocalEndpointSes = (): string => process.env.LOCAL_SES_ENDPOINT || DEFAULT_LOCAL_URL_SES;
+export const getLocalEndpointSes = (): string => process.env.LOCAL_SES_ENDPOINT || DEFAULT_LOCAL_ENDPOINT;
